6492d6487ba265fb3334db2d967614191c667948
whittefang <whittefang@gmail.com>
2016-01-16 20:36:48 -0600

    Game
    Added game files

---
+++ SwingNSwing/Assets/Scripts/SelfTurnOffScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfTurnOffScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	void OnEnable(){
+
+		Invoke("TurnOff", 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void TurnOff(){
+		gameObject.SetActive (false);
+	}
+}
